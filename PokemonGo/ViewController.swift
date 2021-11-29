4c8a652cc17671ca24506d40c488ddfe3ad2efe3
Ricardo Retamozo Garay <ricardo.retamozo@tecsup.edu.pe>
2021-11-29 12:05:11 -0500

    Initial Commit

---
+++ PokemonGo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokemonGo
+//
+//  Created by ricardo on 11/29/21.
+//  Copyright © 2021 empresa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
